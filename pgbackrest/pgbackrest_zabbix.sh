fcd50d352aff80f4b5daac6d73d2a1d70f976af1
Ronald Rood <ronr@ronr.nl>
2022-01-21 17:12:51 +0100

    process all configurations in /etc/pgbackrest/*.conf and allow more stanzas

--- pgbackrest/pgbackrest_zabbix.sh
+++ pgbackrest/pgbackrest_zabbix.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+# read all configs from /etc/pgbackrest/*.conf where each tenant has an own file
+# filename convention: pgbackrest.conf -> default -> tenant ""
+#                      pgbackrest_xxx.conf        -> tenant "xxx"
+# data is sent per tenant to the same key
+# in zabbix 5.0.16 this gives weird behaviour for the character items
+# the items like wal, version and status - that all come from character data
+# become invalid after accepting their data. This is already annoying by itself
+# for zabbix now disrespects the disregard unchanged setting in the items
+# sounds like a zabbix bug to me.
+# since the zabbix_sender limits are not very clear the files are saved
+# in /tmp/pgbackrest_zabbix.data.$tenant.$USER so it can be checked for sizing when
+# problems arrize with a big list of database per config.
+# a database + 1 standby gives about 1500 bytes in the data json.
 
 function missing_tool()
 {
@@ -22,19 +35,25 @@ fi
 date
 NOW=$(date +%s)
 export NOW
+echo
+echo sending discovery structure
+echo
 ls /etc/pgbackrest/*.conf |
-    while read toplevel
-    do
-        echo "toplevel: $toplevel" >&2
-        pgbackrest info --output json --config "$toplevel"
-    done |
-    jq -n '[inputs|.[]]' | # add all arrays from the info commands
-  jq 'map(.archive[] + .backup[] + {name}) 
-    | group_by([.name, .type]) 
-    | map(max_by(.timestamp.stop))
-' | jq --arg NOW "$NOW" -r 'map(.timestamp.age = ($NOW|tonumber) - .timestamp.stop)
-' |
-        tr "\n" " " |                      # key and value, the value being 1 line
+   while read toplevel
+   do
+        tenant=$( (basename "$toplevel")|sed "s/.conf//"|sed "s/.*_//"|sed "s/pgbackrest//")
+        echo "toplevel: $toplevel tenant: $tenant" >&2
+        pgbackrest info --output json --config "$toplevel" |
+        jq --arg tenant "$tenant" 'map([ .backup[] + {name} ])
+        | .[]
+        | [{ tenant: $tenant, name: .[].name, type: .[].type }]
+        | sort
+        | unique
+           '
+   done  |
+    jq -n '[inputs|.[]]' |                 #### all in one big array
+        tee "/tmp/pgbackrest_zabbix.json.$USER" |    # for debugging issues
+        tr "\n" " " |                      # one line
         sed "s/^/- pgbackrest.json /" |    #### add hostname + itemkey. hostname - is taken from config
         if [ "$DEBUG" ]
         then
@@ -42,3 +61,28 @@ ls /etc/pgbackrest/*.conf |
         else
             zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -i -
         fi
+
+
+echo
+echo sending data only latest backup per name per type per tenant
+echo
+ls /etc/pgbackrest/*.conf |
+    while read toplevel
+    do
+        tenant=$( (basename "$toplevel")|sed "s/.conf//"|sed "s/.*_//"|sed "s/pgbackrest//")
+        echo "toplevel: $toplevel tenant: $tenant" >&2
+        pgbackrest info --output json --config "$toplevel"  |
+           jq 'map(.archive[] + .backup[] + {name}) | group_by([.name, .type]) | map(max_by(.timestamp.stop))
+'        | jq --arg tenant "$tenant" --arg NOW "$NOW" -r 'map(.timestamp.age = ($NOW|tonumber) - .timestamp.stop)
+                                                      | map(.tenant = $tenant)
+'        |
+        tee "/tmp/pgbackrest_zabbix.data.$tenant.$USER" |
+        tr "\n" " " |
+        sed "s/^/- pgbackrest.data /" |    #### add hostname + itemkey. hostname - is taken from config
+        if [ "$DEBUG" ]
+        then
+            cat
+        else
+            zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -i -
+        fi
+    done
